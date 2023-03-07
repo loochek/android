96fda6453480f5d8fc8e0fea3c471b0783eba3a1
loochek <m.ziganshin.zm@gmail.com>
2023-03-07 14:16:40 +0300

    Initial commit

---
+++ app/src/main/java/org/loochek/test/MainActivity.kt
@@ -0,0 +1,51 @@
+package org.loochek.test
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.loochek.test.ui.theme.TestAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(applicationContext, "Welcome back!", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(applicationContext, "Goodbye!", Toast.LENGTH_SHORT).show()
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TestAppTheme {
+        Greeting("Android")
+    }
+}
